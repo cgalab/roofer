a4a3e5e7d5c319647765f0b37166b9aae64a450f
Günther Eder <geder@cosy.sbg.ac.at>
2016-04-20 14:50:21 +0200

    -

--- src/Data.h
+++ src/Data.h
@@ -30,75 +30,49 @@
 
 using namespace std;
 
-using K 		  = CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt;
-using Vertex      = K::Vector_2;
-using Point       = K::Point_2;
-using Line        = K::Line_2;
-using Ray         = K::Ray_2;
-using Direction   = K::Direction_2;
+using K 		  = CGAL::Exact_predicates_exact_constructions_kernel;
+using Vector      = K::Vector_3;
+using Point       = K::Point_3;
+using Line        = K::Line_3;
+using Ray         = K::Ray_3;
+using Direction   = K::Direction_3;
 using Polygon     = CGAL::Polygon_2<K>;
+using Plane       = K::Plane_3;
 
 using Transformation = CGAL::Aff_transformationC2<K>;
 
-enum class EventType {EDGE,SPLIT,DIVIDE,CREATE,EMPTY};
+enum class EventType {EDGE,SPLIT,CREATE,MERGE,EMPTY};
 
 struct Config {
 	Config():gui(false),maximize(true),fileName("") {
 		printOptions = "[-min|-max] [-gui] <filename>";
 	}
 
-	bool 		   gui;
-	bool 		   maximize;
+	bool 		gui;
+	bool 		maximize;
 	string      fileName;
 
 	string 		printOptions;
 };
 
-struct WavefrontPoint : public Point, ListItem<WavefrontPoint> {
-	WavefrontPoint(K::FT x, K::FT y)
+struct LineArrangement {
+	LineArrangement(Edge e, )
 	: Point(x,y),start(x,y),time(0),startTime(0),reflex(false),inWavefront(true)	{}
-	WavefrontPoint(Point v)
-	: Point(v),start(v),time(0),startTime(0),reflex(false),inWavefront(true)	{}
-	WavefrontPoint(WavefrontPoint w, Point v)
-	: Point(v),start(w.start),time(w.time),startTime(w.startTime),
-	  velocity(w.velocity),reflex(w.reflex),inWavefront(w.inWavefront)	{}
 
-	Point    start;
-	K::FT    time;
-	K::FT    startTime;
+    vector<Line> lines;
+    vector<int>  sweepLineStatus;
 
-	Vertex   velocity;
-
-	bool 	   reflex;
-	bool	   inWavefront;
-
-	inline void enable()  {inWavefront = true; }
-	inline void disable() {inWavefront = false;}
-	inline bool isInWavefront() {return inWavefront;}
-
-	inline Ray  getRay() {return Ray(*this,velocity);}
-
-	inline void setStartTime(K::FT t) {startTime = t;}
-
-	inline void moveToTime(K::FT newTime) {
-		time  = newTime;
-		*this = WavefrontPoint(*this, start + (velocity * (time - startTime)));
-	}
-
-	inline Point getPointAtTime(K::FT newTime) {
-		return start + (velocity * (newTime - startTime));
-	}
+    
 };
-
-//using WavefrontIterator = CGAL::Polygon_2<K,vector<WavefrontPoint,allocator<WavefrontPoint>>>::Vertex_const_iterator;
 
 /* event location is the point itself.  */
 struct Event : public Point {
 	EventType 	   type;
-	K::FT 		   time;
+	K::FT 		   height;
 
 	//	WavefrontIterator a, b, c;
-	WavefrontPoint *a, *b, *c;
+    Edge e;
+    Intersection e1, e2;
 
 	Event(Point v, EventType t, K::FT _time, WavefrontPoint _a, WavefrontPoint _b, WavefrontPoint _c)
 	: Point(v),type(t),time(_time),a(_a),b(_b),c(_c) {}
@@ -110,12 +84,14 @@ struct Event : public Point {
 
 struct Compare {
     bool operator() (Event &a, Event &b) {
-    	return a.time > b.time;
+    	return a.height > b.height;
     }
 };
 
 using EventQueue = priority_queue<Event,vector<Event>, Compare >;
-using Wavefront  = CGAL::Polygon_2<K,vector<WavefrontPoint> >;
+using Cell       = vector<Point>;
+using Facet      = vector<Cell>;
+using Planes     = vector<CGAL::<K,vector<WavefrontPoint> >;
 
 class Data {
 public:
