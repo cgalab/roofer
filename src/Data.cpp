ff60a6acbedf87e0ca6a63ba56878777b9789ae9
Günther Eder <geder@cosy.sbg.ac.at>
2016-04-20 23:31:33 +0200

    ...

--- src/Data.cpp
+++ src/Data.cpp
@@ -9,12 +9,16 @@
 
 //#include "ImportOBJ.h"
 
+// TEST
+
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Polyhedron_items_3.h>
 #include <CGAL/HalfedgeDS_list.h>
 #include <CGAL/Polyhedron_3.h>
 
-Data::Data():rotateNintyLeft(CGAL::ROTATION, 1, 0) {}
+Data::Data() {}
+
+//Data::Data():rotateNintyLeft(CGAL::ROTATION, 1, 0) {}
 
 Data::~Data() {}
 
@@ -24,13 +28,12 @@ bool Data::loadFile() {
 	return true;
 }
 
-
-WavefrontIterator Data::next(WavefrontIterator i) {
-	return (i+1 == wavefront.vertices_end()) ? wavefront.vertices_begin() : i+1;
+VertexIterator Data::next(VertexIterator i) {
+	return (i+1 == polygon.vertices_end()) ? polygon.vertices_begin() : i+1;
 }
 
-WavefrontIterator Data::prev(WavefrontIterator i) {
-	return (i == wavefront.vertices_begin()) ? wavefront.vertices_end()-1 : i-1;
+VertexIterator Data::prev(VertexIterator i) {
+	return (i == polygon.vertices_begin()) ? polygon.vertices_end()-1 : i-1;
 }
 
 
@@ -45,20 +48,30 @@ bool Data::evaluateArguments(std::list<std::string> args) {
 		std::cout << "Using default test polygon." << std::endl;
 		config.gui = true;
 
-		polygon.push_back( Point(0,0) ) ;
-		polygon.push_back( Point(10,0) ) ;
-		polygon.push_back( Point(12,10) ) ;
-		polygon.push_back( Point(9,9) ) ;
-		polygon.push_back( Point(8,12) ) ;
-		polygon.push_back( Point(7,8) ) ;
-		polygon.push_back( Point(5,11) ) ;
-		polygon.push_back( Point(0,9) ) ;
+        polygon.push_back( K::Point_2(0,0)  );
+		polygon.push_back( K::Point_2(10,0) );
+		polygon.push_back( K::Point_2(12,10));
+		polygon.push_back( K::Point_2(9,9)  );
+		polygon.push_back( K::Point_2(8,12) );
+		polygon.push_back( K::Point_2(7,8)  );
+		polygon.push_back( K::Point_2(5,11) );
+		polygon.push_back( K::Point_2(0,9)  );
 
 
 		for(auto v_i = polygon.vertices_begin(); v_i != polygon.vertices_end(); ++v_i) {
-			auto v_j = v_i->next();
+            auto v_j = next(v_i);
 
-			planes.push_back( Plane(*v_i,*v_j,aösdlfkjasöldfjk 45° ));
+            Point a(v_i->x(),v_i->y(),0);
+            Point b(v_j->x(),v_j->y(),0);
+            
+            auto dir(*v_j - *v_i);
+            K::Vector_2 dirRot(-dir.y(),dir.x());
+            
+            
+            K::Point_2 aTrans(*v_i + dirRot);
+            Point c(aTrans.x(),aTrans.y(),dirRot.squared_length());
+            
+			planes.push_back( Plane( Point(*v_i->x(),*v_,*v_j, *v_i + *v_j /* get 45° */));
 		}
 
 		return true;
--- src/Data.h
+++ src/Data.h
@@ -13,7 +13,6 @@
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Polygon_2.h>
-#include <CGAL/create_straight_skeleton_2.h>
 #include <CGAL/Combinatorial_map_constructors.h>
 
 #include <CGAL/Simple_cartesian.h>
@@ -21,6 +20,8 @@
 #include <CGAL/HalfedgeDS_list.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Linear_cell_complex.h>
+
+#include <CGAL/Arrangement_2.h>
 
 #include <CGAL/intersections.h>
 
@@ -30,15 +31,18 @@
 
 using namespace std;
 
-using K 		   = CGAL::Exact_predicates_exact_constructions_kernel;
-using Vector       = K::Vector_3;
-using Point        = K::Point_3;
-using Line         = K::Line_3;
-using Ray          = K::Ray_3;
-using Direction    = K::Direction_3;
-using Polygon      = CGAL::Polygon_2<K>;
-using Plane        = K::Plane_3;
-using PlanePolygon = vector<Plane>;
+using K 		    = CGAL::Exact_predicates_exact_constructions_kernel;
+using Vector        = K::Vector_3;
+using Point         = K::Point_3;
+using Line          = K::Line_3;
+using Ray           = K::Ray_3;
+using Direction     = K::Direction_3;
+using Polygon       = CGAL::Polygon_2<K>;
+using Plane         = K::Plane_3;
+using PolygonPlanes = vector<Plane>;
+using Edge          = K::Segment_3;
+
+using VertexIterator = Polygon::Vertex_iterator;
 
 using Transformation = CGAL::Aff_transformationC2<K>;
 
@@ -56,31 +60,33 @@ struct Config {
 	string 		printOptions;
 };
 
+// TODO: use arrangement class for this!
 struct LineArrangement {
-	LineArrangement(Edge e, )
-	: Point(x,y),start(x,y),time(0),startTime(0),reflex(false),inWavefront(true)	{}
-
+    LineArrangement(Edge e, PolygonPlanes p)
+    {}
+    
     vector<Line> lines;
     vector<int>  sweepLineStatus;
-
+    
     
 };
 
 /* event location is the point itself.  */
-struct Event : public Point {
+struct Event {
 	EventType 	   type;
+    Point          point;
 	K::FT 		   height;
 
-	//	WavefrontIterator a, b, c;
-    Edge e;
-    Intersection e1, e2;
+    int edgeIndex;
+    int edgeIndexA;
+    int edgeIndexB;
 
-	Event(Point v, EventType t, K::FT _time, WavefrontPoint _a, WavefrontPoint _b, WavefrontPoint _c)
-	: Point(v),type(t),time(_time),a(_a),b(_b),c(_c) {}
-	Event(Point v, EventType t)
-	: Point(v),type(t),time(0),a(NULL),b(NULL),c(NULL) {}
-	Event()
-	: Point(),type(EventType::EMPTY),time(0),a(NULL),b(NULL),c(NULL) {}
+	Event(Point v, EventType t, K::FT _height, int _self, int _a, int _b)
+	: type(t),point(v),height(_height),edgeIndex(_self),edgeIndexA(_a),edgeIndexB(_b) {}
+	//Event(Point v, EventType t)
+	//: point(v),type(t),height(0),edgeIndex(NULL),edgeIndexA(NULL),edgeIndex(NULL) {}
+	//Event()
+	//: Point(),type(EventType::EMPTY),time(0),a(NULL),b(NULL),c(NULL) {}
 };
 
 struct Compare {
@@ -96,20 +102,21 @@ using Facet      = vector<Cell>;
 class Data {
 public:
 	Polygon    	  polygon;
-	PlanePolygon  planes;
+	PolygonPlanes planes;
+    
 	Config        config;
 
 	EventQueue    eventQueue;
 
-	Transformation rotateNintyLeft;
+    //	Transformation rotateNintyLeft;
 
 	Data();
 	virtual ~Data();
 
 	bool evaluateArguments(list<string> args);
 
-	WavefrontIterator next(WavefrontIterator i);
-	WavefrontIterator prev(WavefrontIterator i);
+	VertexIterator next(VertexIterator i);
+	VertexIterator prev(VertexIterator i);
 
 private:
 	bool fileExists(string fileName);
